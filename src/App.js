60145eecfe7d71f0be706667fab9daa253c57297
IharPadbiarezski <yb.igrok.by@gmail.com>
2019-05-18 21:24:31 +0300

    feat: show the message when field of formis empty

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './skeleton.css';
 import Form from './components/Form';
 import { calculateTotal } from './helper';
 import Result from './components/Result';
+import Message from './components/Message';
 
 class App extends Component {
 	state = {
@@ -23,7 +24,17 @@ class App extends Component {
 	};
 
 	render() {
+		// destructuring the state
 		const { term, amount, total } = this.state;
+
+		// Conditionaly Render a Component
+
+		let component;
+		if (total === '') {
+			component = <Message />;
+		} else {
+			component = <Result total={total} amount={amount} term={term} />;
+		}
 		return (
 			<Fragment>
 				<h1>Loan Calculator</h1>
@@ -31,9 +42,7 @@ class App extends Component {
 				<div className="container">
 					<Form loanInformation={this.loanInformation} />
 
-					<div className="messages">
-						<Result total={total} amount={amount} term={term} />
-					</div>
+					<div className="messages">{component}</div>
 				</div>
 			</Fragment>
 		);
---
+++ src/components/Message.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Message = () => <p>Add an Amount and the Term to Repay</p>;
+
+export default Message;
